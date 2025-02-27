b86f39594fd3fe793feaf67b151224dd8f66ea78
sametatabasch <sametatabasch@gmail.com>
2025-02-27 11:01:36 +0300

    index.php eklendi

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ne</title>
+</head>
+<body>
+    <?php 
+        $degisken= "naber";
+
+        echo "ekrana bir şey yazdırır <br>";
+        echo 'değişkenimin derğeri = $degisken<br>';
+        echo "değişkenimin derğeri = $degisken <br>";
+        echo "<h1 id=\"başlık\">sdfşk</h1>";
+        echo '<h1 id="baslik">sadf</h1>';
+        echo "<h1 id=\"başlık\">$degisken</h1>";
+        echo '<h1 id="baslik">'.$degisken.'</h1>';
+        
+    ?>
+</body>
+</html>
